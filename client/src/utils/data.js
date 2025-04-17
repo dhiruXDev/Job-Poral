ca2c82dde9f9536e00f744b3b6b64680511e5799
Dhiraj kumar <dhirajk22410@gmail.com>
2025-04-17 21:46:31 +0530

    Update data.js

--- client/src/utils/data.js
+++ client/src/utils/data.js
@@ -1,4 +1,4 @@
-const BaseURL = "http://localhost:4000/api";
+const BaseURL = "https://job-poral.onrender.com/api";
 
 export const USER_API_END_POINT= `${BaseURL}/user`;
 export const JOB_API_END_POINT= `${BaseURL}/job`
