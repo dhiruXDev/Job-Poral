8b18a25b5c759bf4ef07cc1ad6ccc8af81bdd1c8
Dhiraj kumar <dhirajk22410@gmail.com>
2025-04-17 22:06:59 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: ["https://job-poral-sigma.vercel.app/"],
+  origin: ["https://job-poral-sigma.vercel.app"],
   credentials: true,
 };
 
